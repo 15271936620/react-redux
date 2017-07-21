b16a23500e7d30486ee6002f5afad66da8baab60
15271936620 <2389651246@qq.com>
2017-07-21 13:35:18 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import Provider, { createProvider } from './components/Provider'
 import connectAdvanced from './components/connectAdvanced'
 import connect from './connect/connect'
 
+import {Row} from 'antd'
+
 export { Provider, createProvider, connectAdvanced, connect }
